5f42ee5d0bf2a0303f11768582ed4b9f439082ec
Aníbal Alejandro Catania <anibalacatania@gmail.com>
2022-12-12 00:24:57 -0300

    sesion9

--- sesion9/experimento9.R
+++ sesion9/experimento9.R
@@ -11,13 +11,14 @@ run_panel(
   design_file = "sesion9/sesion9.csv",
   dest_url="192.168.1.123:4001")
 
-
 run_panel(
-  products_file = "sesion7/vinos_7.csv",
+  products_file = "sesion9/vinos_9.csv",
   attributes_file = "atributos.csv",
-  answers_dir = "sesion7/responses/",  
+  answers_dir = "sesion9/responses/",  
   product_name = "CodigoProducto",
-  design_file = "sesion7/sesion7.csv")
+  design_file = "sesion9/sesion9.csv")
+
+
 
 run_board(answers_dir = "sesion9/responses/")
 
